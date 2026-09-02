d1170abf6b0289af32a0e4b243b9f0cd3b337127
Lefurtif221 <thebigmaster2k2@gmail.com>
2026-09-02 01:51:33 +0000

    Remove intro, remove gsap, rebuild dist

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import { useState, useCallback } from 'react';
 import { CartProvider } from './CartContext';
 import Home from './pages/Home';
 import Collections from './pages/Collections';
@@ -8,7 +7,6 @@ import Cart from './pages/Cart';
 import Checkout from './pages/Checkout';
 import Admin from './pages/Admin';
 import Login from './pages/Login';
-import Intro from './components/Intro';
 import './styles/index.css';
 
 function ProtectedRoute({ children }) {
@@ -18,39 +16,27 @@ function ProtectedRoute({ children }) {
 }
 
 export default function App() {
-  const [introDone, setIntroDone] = useState(() => {
-    return sessionStorage.getItem('introDone') === 'true';
-  });
-
-  const handleIntroComplete = useCallback(() => {
-    sessionStorage.setItem('introDone', 'true');
-    setIntroDone(true);
-  }, []);
-
   return (
-    <>
-      {!introDone && <Intro onComplete={handleIntroComplete} />}
-      <CartProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/collections" element={<Collections />} />
-            <Route path="/produit/:id" element={<ProductDetail />} />
-            <Route path="/panier" element={<Cart />} />
-            <Route path="/checkout" element={<Checkout />} />
-            <Route path="/login" element={<Login />} />
-            <Route
-              path="/admin"
-              element={
-                <ProtectedRoute>
-                  <Admin />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
-        </BrowserRouter>
-      </CartProvider>
-    </>
+    <CartProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/produit/:id" element={<ProductDetail />} />
+          <Route path="/panier" element={<Cart />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/login" element={<Login />} />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" replace />} />
+        </Routes>
+      </BrowserRouter>
+    </CartProvider>
   );
 }
