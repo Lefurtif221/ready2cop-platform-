0be2946c76a0336465e530aa259f191d0a2cd8d5
Lefurtif221 <thebigmaster2k2@gmail.com>
2026-09-02 01:56:19 +0000

    Test: add BrowserRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './styles/index.css';
+
+function Home() {
+  return <div style={{ padding: 40, textAlign: 'center' }}><h1>Home - Ready2Cop</h1></div>;
+}
+
+function Collections() {
+  return <div style={{ padding: 40, textAlign: 'center' }}><h1>Collections</h1></div>;
+}
 
 export default function App() {
   return (
-    <div style={{ padding: 40, textAlign: 'center' }}>
-      <h1>Ready2Cop fonctionne !</h1>
-      <p>Si tu vois ce message, React marche.</p>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/collections" element={<Collections />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
